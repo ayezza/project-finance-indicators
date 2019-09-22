3cd5dd90a770acdce3b42933d6d393648e715895
ayezza <ayezza@free.fr>
2019-09-22 11:22:57 +0200

    first project version

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author : Abdel YEZZA (Ph.D)
 This code is completely free and can be modified without any restriction
 Examples given below are presented and explained in my article : QUELQUES NOTIONS DE BASE DE LA FINANCE DES PROJETS
-available at this web site : http://abdel.yezza.free.fr/
+available at this web address : http://abdel.yezza.free.fr/prog/basic_finance_elements-v2.pdf
 """
 
 import sys
